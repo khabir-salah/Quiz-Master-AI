cf4143df8979fe49f703b77e281271226aff619a
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-11 17:55:19 +0100

    class name changed

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enum;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = default!;
+    }
+}
