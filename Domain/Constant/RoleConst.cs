887757a2ead381a3b60e38016745f15b6118ead2
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-11 17:55:39 +0100

    Role Const added

---
+++ Domain/Constant/RoleConst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RoleConst
+{
+    public class  RoleConst
+    {
+        public const string Basic = "Basic";
+        public const string Classic = "Classic";
+        public const string Standard = "Standard";
+    }
+}
