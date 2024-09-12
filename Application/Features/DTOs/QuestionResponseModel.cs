92997bf5915ca135ea7ed6d77e5fc7e9264ce58e
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-12 23:42:13 +0100

    model added

--- Application/Features/DTOs/QuestionResponseModel.cs
+++ Application/Features/DTOs/QuestionResponseModel.cs
@@ -7,22 +7,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Application.Features.DTOs
 {
-    public class QuestionResponseModel
+    public record class QuestionResponseModel
     {
         public string QuestionText { get; set; }
         public ICollection<OptionResponseModel> Options { get; set; }
         public string Answer { get; set; }
         public string Explanation { get; set; }
+        public string UserAnswer { get; set; }
     }
 
-    public class AssessmentResponseModel
+    public record class AssessmentResponseModel
     {
         public ICollection<QuestionResponseModel> Question { get; set; }
         public QuestionType QuestionType { get; set; }
         public DateTime Date { get; set; }
     }
 
-    public class OptionResponseModel
+    public record class OptionResponseModel
     {
         public string Text { get; set; }
     }
