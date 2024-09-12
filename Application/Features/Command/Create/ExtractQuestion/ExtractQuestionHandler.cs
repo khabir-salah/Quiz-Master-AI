63dc76625457c89e4f50beba11c5478cc9a78db3
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-12 23:43:05 +0100

    Extract question feature added

---
+++ Application/Features/Command/Create/ExtractQuestion/ExtractQuestionHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.DTOs;
+using MediatR;
+
+
+namespace Application.Features.Command.Create.ExtractQuestion
+{
+    public class ExtractQuestionHandler(IMediator _mediator) : IRequestHandler<ExtractQuestionCommandModel, BaseResponse<string>>
+    {
+        public Task<BaseResponse<string>> Handle(ExtractQuestionCommandModel request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
