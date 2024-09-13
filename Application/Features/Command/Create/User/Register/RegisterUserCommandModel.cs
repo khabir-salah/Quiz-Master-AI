33b070b111775908cf5e36604329dc52f5c90c3c
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-13 22:47:33 +0100

    User Registration

---
+++ Application/Features/Command/Create/User/Register/RegisterUserCommandModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Command.Create.User.Register
+{
+    public record class  RegisterUserCommandModel(string UserName,  string Password, string Email, string FullName) : IRequest<BaseResponse<string>>
+    {
+    }
+}
---
+++ Application/Features/Command/Create/User/Register/RegisterUserHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.DTOs;
+using Application.Features.Interfaces.IRepositores;
+using Application.Features.Interfaces.IService;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Application.Features.Command.Create.User.Register
+{
+    public class RegisterUserHandler(IGenericRepository<ApplicationUser> _userRepo, UserManager<ApplicationUser> _userManager, IUserService _userService) : IRequestHandler<RegisterUserCommandModel, BaseResponse<string>>
+    {
+        public async Task<BaseResponse<string>> Handle(RegisterUserCommandModel request, CancellationToken cancellationToken)
+        {
+           var isUserExist = await _userRepo.isExist(U => U.Email == request.Email);
+
+           if(!isUserExist)
+           {
+                return new BaseResponse<string>
+                {
+                    IsSuccessful = false,
+                    Result = "User Already Exist"
+                };
+           } 
+
+            var newUser = new ApplicationUser
+            {
+                Email = request.Email,
+                UserName = request.UserName,
+                FullName = request.FullName,
+            };
+            
+            var result = await _userManager.CreateAsync(newUser, request.Password);
+
+            await _userService.AssignBasicRole(newUser);
+
+            return result.Succeeded == true ? new BaseResponse<string> { IsSuccessful = true } : new BaseResponse<string> { IsSuccessful = false };
+        }
+    }
+}
