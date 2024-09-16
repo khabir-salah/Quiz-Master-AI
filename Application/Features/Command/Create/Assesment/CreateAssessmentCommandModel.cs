05c442648fe06b2b99776077207160f9c8cc24ff
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-16 23:00:14 +0100

    assesment creation feature added

---
+++ Application/Features/Command/Create/Assesment/CreateAssessmentCommandModel.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Command.Create.Assesment
+{
+    public record class CreateAssessmentCommandModel
+    {
+        public QuestionType QuestionType { get; set; }
+        public int NumberOfQuestion { get; set; }
+        public Guid UserId { get; set; }
+        public Document Document { get; set; }
+    }
+
+    public record class CreateAssessmentResponseModel
+    {
+        public ICollection<Question> Question { get; set; }
+        public QuestionType QuestionType { get; set; }
+    }
+}
