c89bd78aa983077b8a9f252063416d76cc7b0b2e
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-16 23:02:39 +0100

    get all document added

---
+++ Application/Features/Queries/Document/GetAllDocumentCommandModel.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.Document
+{
+    public record class GetAllDocumentCommandModel : IRequest
+    {
+    }
+
+    public record class DocumentResponseModel
+    {
+        public Guid AssessmentId { get; set; }              
+        public string DocumentName { get; set; }
+        public int 
+    }
+}
