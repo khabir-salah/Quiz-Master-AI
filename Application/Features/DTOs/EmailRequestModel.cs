032309b06553334def901dc51f65202184d5110a
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-16 23:00:31 +0100

    dto added

---
+++ Application/Features/DTOs/EmailRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DTOs
+{
+    public class EmailRequestModel
+    {
+        public string ToEmail { get; set; }
+        public string ToName { get; set; }
+        public string Subject { get; set; }
+        public string HtmlContent { get; set;}
+    }
+}
---
+++ Application/Features/DTOs/LoginRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DTOs
+{
+    public class LoginRequestModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponseModel
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+    }
+}
