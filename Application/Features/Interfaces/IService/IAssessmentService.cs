ed6e5adb1de7ad9826f44946a396f543d508e615
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-17 23:58:49 +0100

    assesment feature added

---
+++ Application/Features/Interfaces/IService/IAssessmentService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Interfaces.IService
+{
+    public interface IAssessmentService
+    {
+        Task StoreQuizGeneration(string userId, DateTime date);
+        Task<int> GetAssessmentGeneratedToday(string userId, DateTime date);
+    }
+}
---
+++ Application/Features/Queries/Services/Implementation/AssessmentService.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Interfaces.IRepositores;
+using Application.Features.Interfaces.IService;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.Services.Implementation
+{
+    public class AssessmentService(IGenericRepository<Assesment> _assessment) : IAssessmentService
+    {
+        public async  Task<int> GetAssessmentGeneratedToday(string userId, DateTime date)
+        {
+            var today = date.Date;
+            var record = await _assessment.GetAsync(q => q.ApplicationUserId == userId && q.Date == today);
+
+            return record?.AssessmentCount ?? 0;
+        }
+
+        public async Task StoreQuizGeneration(string userId, DateTime date)
+        {
+            var today = date.Date;
+            var record = await _assessment.GetAsync(q => q.ApplicationUserId == userId && q.Date == today);
+            record.AssessmentCount += 1;
+        }
+    }
+}
