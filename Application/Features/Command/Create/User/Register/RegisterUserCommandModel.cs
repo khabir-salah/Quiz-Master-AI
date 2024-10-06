1246c743ea83df796c9ca2a509d12aaae8dc5b06
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-06 20:29:06 +0100

    register user feature modified

--- Application/Features/Command/Create/User/Register/RegisterUserCommandModel.cs
+++ Application/Features/Command/Create/User/Register/RegisterUserCommandModel.cs
@@ -8,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Command.Create.User.Register
 {
-    public record class  RegisterUserCommandModel(  string Password, string Email, string LastName, string FirstName) : IRequest<BaseResponse<string>>
+    public record class  RegisterUserCommandModel(string Password, string Email, string LastName, string FirstName) : IRequest<BaseResponse<string>>
     {
+        //public RegistrationRequest RegisterRequest {  get; set; }
+        //public string origin { get; set; }
     }
+
+    //public record class RegistrationRequest(string Password, string Email, string LastName, string FirstName, bool AutoActivateEmail, bool ActivateUser);
 }
--- Application/Features/Command/Create/User/Register/RegisterUserHandler.cs
+++ Application/Features/Command/Create/User/Register/RegisterUserHandler.cs
@@ -2,39 +2,65 @@
 using Application.Features.Interfaces.IRepositores;
 using Application.Features.Interfaces.IService;
 using Domain.Entities;
+using Domain.RoleConst;
+using Hangfire;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using System.ComponentModel;
+using System.Text;
 
 
 namespace Application.Features.Command.Create.User.Register
 {
-    public class RegisterUserHandler(IGenericRepository<ApplicationUser> _userRepo, UserManager<ApplicationUser> _userManager, IUserService _userService) : IRequestHandler<RegisterUserCommandModel, BaseResponse<string>>
+    public class RegisterUserHandler(IGenericRepository<ApplicationUser> _userRepo, UserManager<ApplicationUser> _userManager, IUserService _userService, IEmailService _emailService, ISeedRole _role, IBackgroundJobClient _backgroundJobClient) : IRequestHandler<RegisterUserCommandModel, BaseResponse<string>>
     {
         public async Task<BaseResponse<string>> Handle(RegisterUserCommandModel request, CancellationToken cancellationToken)
         {
-           var isUserExist = await _userRepo.isExist(U => U.Email == request.Email);
+           var isUserExist = await _userManager.FindByEmailAsync(request.Email);
 
-           if(!isUserExist)
+           if(isUserExist != null)
            {
                 return new BaseResponse<string>
                 {
                     IsSuccessful = false,
-                    Result = "User Already Exist"
+                    Message = "User Already Exist"
                 };
-           } 
+            }
+
+            var origin = Url.Uri;
 
             var newUser = new ApplicationUser
             {
                 Email = request.Email,
                 FirstName = request.FirstName,
                 LastName = request.LastName,
+                UserName = request.FirstName
             };
             
             var result = await _userManager.CreateAsync(newUser, request.Password);
+            if(result.Succeeded)
+            {
+                await _role.AssignBasicRole(newUser);
+                var verificationUri = await SendEmailVerifaction(newUser, origin);
+                _backgroundJobClient.Enqueue(() => _emailService.SendWelcomeMessage(request.Email, newUser.FullName, verificationUri));
+                return new BaseResponse<string> { IsSuccessful = true, Message = "User Registered please check your mailbox to verify" };
+                
+            }
+            return result.Succeeded == true ? new BaseResponse<string> { IsSuccessful = true } : new BaseResponse<string> { IsSuccessful = false, Message = string.Join(",   ", result.Errors.Select(e => e.Description)) };
+        }
 
-            await _userService.AssignBasicRole(newUser);
 
-            return result.Succeeded == true ? new BaseResponse<string> { IsSuccessful = true } : new BaseResponse<string> { IsSuccessful = false };
+        private async Task<string> SendEmailVerifaction(ApplicationUser user,  string origin)
+        {
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var route = "api/Account/confirm-email/";
+            var endpointUri = new Uri(string.Concat($"{origin}/", route));
+            var verificationUri = QueryHelpers.AddQueryString(endpointUri.ToString(), "userId", user.Id);
+            verificationUri = QueryHelpers.AddQueryString(verificationUri, "code", code);
+            return verificationUri;
         }
     }
 }
