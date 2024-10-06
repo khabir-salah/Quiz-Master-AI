bddbbadc15d999246bd7d6018895f3b436a5c8ae
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-06 20:30:58 +0100

    Role added

---
+++ Application/Features/Interfaces/IService/ISeedRole.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Interfaces.IService
+{
+    public interface ISeedRole
+    {
+        Task AssignStandardRole(ApplicationUser user);
+        Task AssignClassicRole(ApplicationUser user);
+        Task AssignBasicRole(ApplicationUser user);
+    }
+}
---
+++ Application/Features/Queries/Services/Implementation/SeedRole.cs
@@ -0,0 +1,55 @@
+﻿using Application.Features.Interfaces.IService;
+using Domain.Entities;
+using Domain.RoleConst;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.Services.Implementation
+{
+    public  class SeedRole(UserManager<ApplicationUser> _userManager) : ISeedRole
+    { 
+        public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!await roleManager.RoleExistsAsync(RoleConst.Basic))
+            {
+                await roleManager.CreateAsync(new IdentityRole(RoleConst.Basic));
+            }
+            if (!await roleManager.RoleExistsAsync(RoleConst.Standard))
+            {
+                await roleManager.CreateAsync(new IdentityRole(RoleConst.Standard));
+            }
+            if (!await roleManager.RoleExistsAsync(RoleConst.Classic))
+            {
+                await roleManager.CreateAsync(new IdentityRole(RoleConst.Classic));
+            }
+        }
+
+        public async Task AssignBasicRole(ApplicationUser user)
+        {
+            if (!await _userManager.IsInRoleAsync(user, RoleConst.Basic))
+            {
+                await _userManager.AddToRoleAsync(user, RoleConst.Basic);
+            }
+        }
+
+        public async Task AssignClassicRole(ApplicationUser user)
+        {
+            if (!await _userManager.IsInRoleAsync(user, RoleConst.Classic))
+            {
+                await _userManager.AddToRoleAsync(user, RoleConst.Classic);
+            }
+        }
+
+        public async Task AssignStandardRole(ApplicationUser user)
+        {
+            if (!await _userManager.IsInRoleAsync(user, RoleConst.Standard))
+            {
+                await _userManager.AddToRoleAsync(user, RoleConst.Standard);
+            }
+        }
+    }
+}
