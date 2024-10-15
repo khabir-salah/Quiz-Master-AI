e90de7df6578b44a8ce9574fb111e60f8530d4df
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-15 23:43:36 +0100

    Iservoice added

--- Infrastructure/Extension/ServiceCollectionExtension.cs
+++ Infrastructure/Extension/ServiceCollectionExtension.cs
@@ -1,13 +1,10 @@
-﻿using Application.Features.Command.Create.Assesment;
-using Domain.Entities;
+﻿using Application.Features.Interfaces.IRepositores;
+using Application.Features.Interfaces.IService;
+using Application.Features.Queries.RetakeAssessment;
+using Application.Features.Queries.Services.Implementation;
+using Infrastructure.Persistent.Repositories.Implementation;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Extension
 {
@@ -15,13 +12,40 @@ namespace Infrastructure.Extension
     {
         public static void  AddRepository(this IServiceCollection services)
         {
+            services.AddScoped<IUserRepository, UserRepository>()
+                .AddScoped<IAssessmentRepository, AssessmentRepository>()
+                .AddScoped<IDocumentRepository, DocumentRepository>()
+                .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
 
+        public static void AddService(this IServiceCollection services)
+        {
+            services.AddScoped<IAssessmentService, AssessmentService>()
+                .AddScoped<IExtractQuestionService, ExtractQuestionService>()
+                .AddScoped<ITextGenerator, TextGenerator>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<IEmailService, EmailService>()
+                .AddScoped<ITextAssessmentService, TextAssessmentService>()
+                .AddScoped<ICohereService, CohereService>()
+                .AddScoped<ISeedRole, SeedRole>();
         }
 
         public static void AddMediatRs(this IServiceCollection services)
         {
-            services.AddMediatR(mR => mR.RegisterServicesFromAssemblies(typeof(TextAssessmentHandler).Assembly));
+            services.AddMediatR(mR => mR.RegisterServicesFromAssemblies(typeof(RetakeAssessmentHandler).Assembly));
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 3;
+            });
+
         }
 
+        
     }
 }
